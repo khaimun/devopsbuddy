6ebc3dcd5e77df0e18e3798a674d99391e313471
khaimun <cheangkhaimun@gmail.com>
2018-04-30 00:45:45 +0800

    added placeholder

---
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.devopsbuddy.web.controllers;
+
+/**
+ * Created by someone
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
+}
